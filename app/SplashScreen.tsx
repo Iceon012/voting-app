d4ae5cf28def10d6d6dccab5db4d135b66c86192
Arvin Ayson <a.ayson12345@gmail.com>
2024-10-26 12:35:04 +0800

    Apply Splash Screen

---
+++ app/SplashScreen.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from "react";
+import { View, Image, Animated, Dimensions, StyleSheet } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export default function SplashScreen({
+  onAnimationComplete,
+}: {
+  onAnimationComplete: () => void;
+}) {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0.5)).current;
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      }),
+      Animated.spring(scaleAnim, {
+        toValue: 1,
+        friction: 4,
+        tension: 10,
+        useNativeDriver: true,
+      }),
+    ]).start(() => {
+      // Animation complete
+      setTimeout(onAnimationComplete, 500); // Wait for 500ms before calling onAnimationComplete
+    });
+  }, [fadeAnim, scaleAnim, onAnimationComplete]);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.logoContainer,
+          {
+            opacity: fadeAnim,
+            transform: [{ scale: scaleAnim }],
+          },
+        ]}
+      >
+        <Image
+          source={{
+            uri: "https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg",
+          }}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logoContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: width * 0.5,
+    height: height * 0.2,
+  },
+});
